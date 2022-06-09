9cb1b6351e724b16f84bc1be439af61d8c857a57
Pavel Lebedev <pavel.lebedev@whitetigersoft.ru>
2022-06-10 00:49:49 +0300

    console application

--- App.php
+++ App.php
@@ -2,13 +2,18 @@
 include 'DBConnection.php';
 include 'DBConnectionParams.php';
 include 'Application.php';
+include 'migration/Migrate.php';
 
 $dbConn = new DBConnection(new DBConnectionParams());
-$do = new Application($dbConn);
 
-if (empty($argv[1])) {
-    $do->viewList();
+if (!empty($argv[1]) && $argv[1] == 'migrate') {
+    $do = new Migrate($dbConn);
 } else {
-    $do->addCommand($argv);
+    $do = new Application($dbConn);
+    if (empty($argv[1])) {
+        $do->viewList();
+    } else {
+        $do->addCommand($argv);
+    }
 }
 echo "\n";
